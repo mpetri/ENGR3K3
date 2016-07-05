f09ac2e94e3853ee5338ac1e2f76a62672354c63
Matthias Petri <Matthias.Petri@gmail.com>
2016-07-05 10:29:21 +1000

    more files and clang format

--- L2/linkedlist.c
+++ L2/linkedlist.c
@@ -3,25 +3,25 @@
 #include <assert.h>
 
 typedef struct {
-	int r;
-	int g;
-	int b;
+    int r;
+    int g;
+    int b;
 } data_t;
 
 typedef struct node node_t;
 
 struct node {
     data_t data;
-    node_t *next;
+    node_t* next;
 };
 
-void
-process(data_t d) {
-	printf("(%.3d,%.3d,%.3d)\n",d.r,d.g,d.b);
+void process(data_t d)
+{
+    printf("(%.3d,%.3d,%.3d)\n", d.r, d.g, d.b);
 }
 
-void 
-process_each(node_t *p) {
+void process_each(node_t* p)
+{
     while (p) {
         process(p->data);
         p = p->next;
@@ -29,8 +29,9 @@ process_each(node_t *p) {
 }
 
 node_t*
-push(data_t stuff, node_t* p) {
-    node_t* new = (node_t*) malloc(sizeof(node_t));
+push(data_t stuff, node_t* p)
+{
+    node_t* new = (node_t*)malloc(sizeof(node_t));
     assert(new);
     new->data = stuff;
     new->next = p;
@@ -38,34 +39,35 @@ push(data_t stuff, node_t* p) {
 }
 
 node_t*
-pop(node_t *p) {
-	node_t* old;
-	assert(p);
-	old = p;
-	p = p->next;
-	free(old);
-	return p;
+pop(node_t* p)
+{
+    node_t* old;
+    assert(p);
+    old = p;
+    p = p->next;
+    free(old);
+    return p;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
-	node_t* head = NULL;
+    node_t* head = NULL;
 
-	for(int i=0;i<50;i++) {
-		data_t new_dat;
-		new_dat.r = rand() % 255;
-		new_dat.g = rand() % 255;
-		new_dat.b = rand() % 255;
-		head = push(new_dat,head);
-	}
+    for (int i = 0; i < 50; i++) {
+        data_t new_dat;
+        new_dat.r = rand() % 255;
+        new_dat.g = rand() % 255;
+        new_dat.b = rand() % 255;
+        head = push(new_dat, head);
+    }
 
-	/* process all elements in list */
-	process_each(head);
+    /* process all elements in list */
+    process_each(head);
 
-	/* remove everything */
-	while(head) {
-		head = pop(head);
-	}
-	
-	return 0;
+    /* remove everything */
+    while (head) {
+        head = pop(head);
+    }
+
+    return 0;
 }
--- L2/realloc.c
+++ L2/realloc.c
@@ -3,49 +3,50 @@
 
 #define INITIAL_SIZE 128
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
-	int buf_size = INITIAL_SIZE;
-	int* data = (int*) malloc(sizeof(int)*buf_size);
-	if(data == NULL) {
-		printf("error allocating memory!\n");
-		exit(EXIT_FAILURE);
-	}
-	/* read the data */
-	int num = 0;
-	int read = 0;
-	while( scanf("%d",&num) == 1 ) {
-		if(read+1 == buf_size) {
-			buf_size = buf_size * 2;
-			data = realloc(data,sizeof(int)*buf_size);
-			if(data == NULL) {
-				printf("error allocating memory!\n");
-				exit(EXIT_FAILURE);
-			}
-			printf("realloc to %d successful\n",buf_size);
-		}
-		data[read++] = num;
-	}
-	/* shrink to correct size */
-	data = realloc(data,read*sizeof(int));
-	if(data == NULL) {
-		printf("error shrinking memory!\n");
-		exit(EXIT_FAILURE);
-	} else {
-		printf("shrink to %d successful\n",read);
-	}
+    int buf_size = INITIAL_SIZE;
+    int* data = (int*)malloc(sizeof(int) * buf_size);
+    if (data == NULL) {
+        printf("error allocating memory!\n");
+        exit(EXIT_FAILURE);
+    }
+    /* read the data */
+    int num = 0;
+    int read = 0;
+    while (scanf("%d", &num) == 1) {
+        if (read + 1 == buf_size) {
+            buf_size = buf_size * 2;
+            data = realloc(data, sizeof(int) * buf_size);
+            if (data == NULL) {
+                printf("error allocating memory!\n");
+                exit(EXIT_FAILURE);
+            }
+            printf("realloc to %d successful\n", buf_size);
+        }
+        data[read++] = num;
+    }
+    /* shrink to correct size */
+    data = realloc(data, read * sizeof(int));
+    if (data == NULL) {
+        printf("error shrinking memory!\n");
+        exit(EXIT_FAILURE);
+    }
+    else {
+        printf("shrink to %d successful\n", read);
+    }
 
-	/* NOW DO STUFF WITH DATA */
-	int sum = 0;
-	int64_t sum64 = 0;
-	for(int i=0;i<read;i++) {
-		sum += data[i];
-		sum64 += data[i];
-	}
-	printf("sum = %d\n",sum); // OVERFLOW!
-	printf("sum64 = %ld\n",sum64);
- 
-	/* always free at the end */
-	free(data);
-	return 0;
+    /* NOW DO STUFF WITH DATA */
+    int sum = 0;
+    int64_t sum64 = 0;
+    for (int i = 0; i < read; i++) {
+        sum += data[i];
+        sum64 += data[i];
+    }
+    printf("sum = %d\n", sum); // OVERFLOW!
+    printf("sum64 = %ld\n", sum64);
+
+    /* always free at the end */
+    free(data);
+    return 0;
 }
