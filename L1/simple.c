c81b4bcb18d3b4d131ede871d51bb2d4aa809485
Matthias Petri <Matthias.Petri@gmail.com>
2016-07-05 10:32:03 +1000

    clang format

--- L1/simple.c
+++ L1/simple.c
@@ -9,38 +9,38 @@ typedef struct {
     float y;
 } point_t;
 
-
-int main() {
+int main()
+{
     /* declare variables */
     point_t A[MAX_POINTS];
     point_t* a_ptr = &A[0];
     int points_read = 0;
     /* read floats from stdin */
-    while( points_read < MAX_POINTS && 
-        scanf("%f %f\n",
-            &(a_ptr->x),&(a_ptr->y)) == 2 ) 
-    {
+    while (points_read < MAX_POINTS && scanf("%f %f\n", &(a_ptr->x), &(a_ptr->y)) == 2) {
         printf("read (%f,%f)\n",
-            a_ptr->x,a_ptr->y);
-        points_read++; a_ptr++;
+            a_ptr->x, a_ptr->y);
+        points_read++;
+        a_ptr++;
     }
-    if(points_read == 0) {
+    if (points_read == 0) {
         printf("no points read\n");
         return 0;
     }
 
     /* find */
-    float max_x = A[0].x; 
+    float max_x = A[0].x;
     float min_y = A[0].y;
     float sum = 0.0f;
-    for(int i=1;i<points_read;i++) {
-        if(max_x < A[i].x) max_x = A[i].x;
-        if(min_y > A[i].y) min_y = A[i].y;
+    for (int i = 1; i < points_read; i++) {
+        if (max_x < A[i].x)
+            max_x = A[i].x;
+        if (min_y > A[i].y)
+            min_y = A[i].y;
         sum += A[i].x + A[i].y;
     }
     /* print stats */
     printf("%.3f %.3f %.3f\n",
-        min_y,max_x,sum);
+        min_y, max_x, sum);
 
     return 0;
 }
