1b9841d2ead5100939775576fd0b0498b46f83ad
Matthias Petri <Matthias.Petri@gmail.com>
2016-07-05 11:31:04 +1000

    linked list

--- L2/linkedlist.c
+++ L2/linkedlist.c
@@ -6,21 +6,30 @@ typedef struct {
     int r;
     int g;
     int b;
-} data_t;
+} rgb_t;
 
 typedef struct node node_t;
 
 struct node {
-    data_t data;
+    void* data;
     node_t* next;
 };
 
-void process(data_t d)
+typedef struct {
+    int num_elements;
+    node_t* head;
+    node_t* tail;
+    void (*del)(void*);
+} list_t;
+
+void rgb_print(void* d)
 {
-    printf("(%.3d,%.3d,%.3d)\n", d.r, d.g, d.b);
+	assert(d != NULL);
+	rgb_t* rd = (rgb_t*)d;
+    printf("(%.3d,%.3d,%.3d)\n", rd->r, rd->g, rd->b);
 }
 
-void process_each(node_t* p)
+void list_process_all(node_t* p,void (*process)(void*))
 {
     while (p) {
         process(p->data);
@@ -28,46 +37,123 @@ void process_each(node_t* p)
     }
 }
 
-node_t*
-push(data_t stuff, node_t* p)
+void* list_pop_front(list_t* list)
 {
-    node_t* new = (node_t*)malloc(sizeof(node_t));
-    assert(new);
-    new->data = stuff;
-    new->next = p;
-    return new;
+	assert(list != NULL);
+	assert(list->num_elements > 0);
+    node_t* old;
+    assert(list->head != NULL);
+    old = list->head;
+    list->head = list->head->next;
+    void* d = old->data;
+    free(old);
+    list->num_elements--;
+    if (list->num_elements == 0) {
+        list->head = NULL;
+        list->tail = NULL;
+    }
+    return d;
 }
 
-node_t*
-pop(node_t* p)
+void list_push_front(list_t* list, void* d)
 {
-    node_t* old;
-    assert(p);
-    old = p;
-    p = p->next;
-    free(old);
-    return p;
+	assert(list != NULL);
+    node_t* new = (node_t*)malloc(sizeof(node_t));
+    assert(new);
+    new->data = d;
+    new->next = list->head;
+    list->head = new;
+    if (list->tail == NULL)
+        list->tail = new;
+    list->num_elements++;
+}
+
+void list_push_back(list_t* list, void* d)
+{
+	assert(list != NULL);
+    node_t* new = (node_t*)malloc(sizeof(node_t));
+    assert(new != NULL);
+    new->data = d;
+    new->next = NULL;
+    if (list->tail)
+        list->tail->next = new;
+    list->tail = new;
+    if (list->head == NULL)
+        list->head = new;
+    list->num_elements++;
+}
+
+/* TODO: implement a function inserts a new node (for item d)
+		 after node n
+*/
+void list_insert_after(list_t* list, node_t* n, void* d)
+{
+    assert(list != NULL);
+    assert(n != NULL);
+}
+
+/* TODO: implement a function which removes the
+		 last element in the list 
+*/
+void* list_pop_back(list_t* list)
+{
+	assert(list != NULL);
+	assert(list->num_elements > 0);
+	return NULL;
+}
+
+list_t* list_new(void (*delfunc)(void*))
+{
+    list_t* list;
+    list = (list_t*)malloc(sizeof(list_t));
+    assert(list != NULL);
+    list->head = NULL;
+    list->tail = NULL;
+    list->num_elements = 0;
+    list->del = delfunc;
+    return list;
+}
+
+void list_free(list_t* list)
+{
+    while (list->num_elements) {
+        void* elem = list_pop_front(list);
+        list->del(elem);
+    }
+    free(list);
 }
 
 int main(int argc, char const* argv[])
 {
-    node_t* head = NULL;
+	/* create new list object with regular 'free' as the del function */
+    list_t* list = list_new(free);
 
-    for (int i = 0; i < 50; i++) {
-        data_t new_dat;
-        new_dat.r = rand() % 255;
-        new_dat.g = rand() % 255;
-        new_dat.b = rand() % 255;
-        head = push(new_dat, head);
+    /* insert some elements at the front */
+    assert(list->num_elements == 0);
+    for (int i = 0; i < 5; i++) {
+        rgb_t* new_dat = (rgb_t*) malloc(sizeof(rgb_t));
+        new_dat->r = rand() % 255;
+        new_dat->g = rand() % 255;
+        new_dat->b = rand() % 255;
+        list_push_back(list, new_dat);
     }
+    assert(list->num_elements == 5);
+
+    /* insert some elements at the back */
+    for (int i = 0; i < 5; i++) {
+        rgb_t* new_dat = (rgb_t*) malloc(sizeof(rgb_t));
+        new_dat->r = rand() % 255;
+        new_dat->g = rand() % 255;
+        new_dat->b = rand() % 255;
+        list_push_front(list, new_dat);
+    }
+    assert(list->num_elements == 10);
 
     /* process all elements in list */
-    process_each(head);
+    list_process_all(list->head,rgb_print);
 
     /* remove everything */
-    while (head) {
-        head = pop(head);
-    }
+    list_free(list);
 
     return 0;
 }
